cf9d866374cd9155cd20984ffb798b91c44f413d
LECONTE ESTEBAN <esleconte@mx21>
2024-03-12 09:19:04 +0100

    import pokemon fini

--- pages/js/import.js
+++ pages/js/import.js
@@ -2,10 +2,21 @@ function import_pokemon(){
   for(let pok of pokemon){
   
     if(pok.form == "Normal"){
-      var imp_pokemon = new Pokemon(pok.pokemon_id,pok.pokemon_name,pok.base_defense,pok.base_attack,pok.base_stamina,"jnvreu","rmkmkb","oufrh","ejrf","uer");
-      //console.log(imp_pokemon.toString());
-      var all_pok = new List_pokemon(imp_pokemon);
-      console.log(all_pok.all_pokemon);
+      for(let pok_move of pokemon_moves){
+        if(pok_move.form == "Normal"){
+          if(pok_move.pokemon_id == pok.pokemon_id){
+            for(let pok_types of pokemon_types){
+              if(pok_types.form == "Normal"){
+                if(pok_types.pokemon_id == pok.pokemon_id){
+                  var imp_pokemon = new Pokemon(pok.pokemon_id,pok.pokemon_name,pok.base_defense,pok.base_attack,pok.base_stamina,pok_types.type,pok_move.charged_moves,pok_move.fast_moves,pok_move.elite_charged_moves,pok_move.elite_fast_moves);
+                  console.log(imp_pokemon.toString());
+                  var all_pok = new List_pokemon(imp_pokemon);
+                }
+              }
+            }
+          }
+        }
+      }
     }
     
   }
