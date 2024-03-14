24b2ae823846ed09c280b0482bc0393dfd3502bc
FILAUX NATHAN <nfilaux@mx21>
2024-03-14 11:11:16 +0100

    vrai dict

--- pages/js/import.js
+++ pages/js/import.js
@@ -1,7 +1,7 @@
 function import_pokemon(){
-  let tab_types = [];
+  let tab_types = {};
   let tab_nom_types = [];
-  let tab_poke = [];
+  let tab_poke = {};
   let obj_type;
 
 
@@ -11,22 +11,27 @@ function import_pokemon(){
 
   for(let nom_type of tab_nom_types){
     obj_type = new Type(nom_type, type_effectiveness[nom_type]);
-    tab_types.push(obj_type);
+    tab_types[nom_type] = obj_type;
   }
   Type.all_types = tab_types;
 
   for(let pok of pokemon){
 
     if(pok.form == "Normal"){
-      types = null;
+      types_raw = null;
       for(let poke_t of pokemon_types){
         if(poke_t.form == "Normal" && poke_t.pokemon_id == pok.pokemon_id){
-          types = poke_t.type
+          types_raw = poke_t.type
         }
       }
-
-      let imp_pokemon = new Pokemon(pok.pokemon_id,pok.pokemon_name,pok.base_defense,pok.base_attack,pok.base_stamina,type,"rmkmkb","oufrh","ejrf","uer");
-      tab_poke.push({ pokemon_id:imp_pokemon});
+      types = []
+      for(let t of types_raw){
+        types += Type.all_types[t]
+      }
+      
+      let imp_pokemon = new Pokemon(pok.pokemon_id,pok.pokemon_name,pok.base_defense,pok.base_attack,pok.base_stamina,types,"rmkmkb","oufrh","ejrf","uer");
+      //tab_poke.push({ pokemon_id:imp_pokemon});
+      tab_poke[pok.pokemon_id] = imp_pokemon;
     }
   }
   Pokemon.all_pokemon = tab_poke;
