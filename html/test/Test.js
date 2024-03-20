fc911dac469acc85d13c779f8638ec1ff9e77925
FILAUX NATHAN <nfilaux@mx21>
2024-03-20 17:46:00 +0100

    debut weakest enemies

--- html/test/Test.js
+++ html/test/Test.js
@@ -1,12 +1,11 @@
 function getPokemonsByType(typeName){
-    import_pokemon();
-    all_pokemon_type = [];
-    for (pok in Pokemon.all_pokemon){
+    let all_pokemon_type = [];
+    for (pok in Pokemon.all_pokemons){
         //console.log(Pokemon.all_pokemon[pok]._types);
-        for(type in Pokemon.all_pokemon[pok]._types ){
+        for(type in Pokemon.all_pokemons[pok]._types ){
             //console.log(Pokemon.all_pokemon[pok]._types[type].nom);
-            if (Pokemon.all_pokemon[pok]._types[type].nom == typeName){
-                all_pokemon_type.push(Pokemon.all_pokemon[pok]); 
+            if (Pokemon.all_pokemons[pok]._types[type].nom == typeName){
+                all_pokemon_type.push(Pokemon.all_pokemons[pok]); 
             }
         }
     }
@@ -14,11 +13,9 @@ function getPokemonsByType(typeName){
 }
 
 function getPokemonsByAttack(attackName){
-    import_pokemon();
-    all_pokemon_attack = [];
+    let all_pokemon_attack = [];
 
-    for (pok in Pokemon.all_pokemon){
-        console.log(Pokemon.all_pokemon[pok]);
+    for (pok in Pokemon.all_pokemons){
         /*for(type in Pokemon.all_pokemon[pok]._types ){
             //console.log(Pokemon.all_pokemon[pok]._types[type].nom);
             if (Pokemon.all_pokemon[pok]._types[type].nom == typeName){
@@ -30,5 +27,35 @@ function getPokemonsByAttack(attackName){
 }
 
 function getAttacksByType(typeName){
-    
+    let all_attack_by_type = [];
+    for(current in Attack.all_attacks){
+        if(Attack.all_attacks[current].type.nom == typeName){
+            all_attack_by_type.push(Attack.all_attacks[current]);
+        }
+    }
+    return all_attack_by_type;
+}
+
+
+function getWeakestEnemies(attack){
+    for(let current in Attack.all_attacks){
+        if(Attack.all_attacks[current].nom == attack){
+            var type_attack = Attack.all_attacks[current].type
+        }
+        break;
+    }
+
+    let tab_worst_types = []
+    // ajout type simple
+    for(let type in Type.all_types){
+        tab_worst_types.push({'type': Type.all_types[type].nom , 'effec': type_attack.effectiveness(Type.all_types[type].nom)})
+    }
+    console.log(type_attack);
+
+
+    return 0;
+}
+
+function getBestAttackTypesForEnemy(name){
+    return 0;
 }
