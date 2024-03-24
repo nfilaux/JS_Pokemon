e478ae3d49a7571754b742b59f6c2d0242d17d66
Nathan <n.filaux@gmail.com>
2024-03-24 13:08:41 +0100

    fin phase 1

--- html/data/Pokemon.js
+++ html/data/Pokemon.js
@@ -1,35 +1,36 @@
-class Pokemon{
+class Pokemon {
+  // variable de classe stockant tout les pokemons
   static _all_pokemons;
-	constructor (pokemon_id, pokemon_name, base_defense, base_attack, base_stamina, types, attacks) {
-		this._pokemon_id = pokemon_id
+  constructor(pokemon_id, pokemon_name, base_defense, base_attack, base_stamina, types, attacks) {
+    this._pokemon_id = pokemon_id
     this._pokemon_name = pokemon_name
 
     this._base_defense = base_defense
     this._base_attack = base_attack
     this._base_stamina = base_stamina
-
+    // les types sont stockés sous la forme d'un tableau ex : [type1, type2] ou [type]
     this._types = types
-
+    // les attaques sont stockés sous la forme d'un tableau ex : [attack1, attack2, attack3, attack4]
     this._attacks = attacks
-	}
-  
-  get pokemon_id() {return this._pokemon_id;}
-  get pokemon_name() {return this._pokemon_name;}
+  }
 
-  get base_defense() {return this._base_defense;}
-  get base_attack() {return this._base_attack;}
-  get base_stamina() {return this._base_stamina;}
+  get pokemon_id() { return this._pokemon_id; }
+  get pokemon_name() { return this._pokemon_name; }
 
-  get types() {return this._types;}
+  get base_defense() { return this._base_defense; }
+  get base_attack() { return this._base_attack; }
+  get base_stamina() { return this._base_stamina; }
 
-  get attacks() {return this._attacks;}
+  get types() { return this._types; }
+
+  get attacks() { return this._attacks; }
 
 
-  static get all_pokemons(){return Pokemon._all_pokemons}
+  static get all_pokemons() { return Pokemon._all_pokemons }
+  static set all_pokemons(all_poke) { Pokemon._all_pokemons = all_poke }
 
-  static set all_pokemons(all_poke){Pokemon._all_pokemons = all_poke}
-  
-  toString() {return `[
+  toString() {
+    return `[
     pokemon_id = ${this.pokemon_id},\n
     pokemon_name = ${this.pokemon_name},\n
     base_defense = ${this.base_defense},\n
@@ -40,6 +41,7 @@ class Pokemon{
     fast_moves = [${this.fast_moves}],\n
     elite_charged_moves = [${this.elite_charged_moves}],\n
     elite_fast_moves = [${this.elite_fast_moves}]\n
-  ]`;}
+  ]`;
+  }
 
 }
--- html/data/import.js
+++ html/data/import.js
@@ -1,4 +1,4 @@
-function import_pokemon(){
+function import_pokemon() {
   // Initialise des tableaux et des objets pour stocker les données.
   let tab_types = {};
   let tab_nom_types = [];
@@ -13,34 +13,34 @@ function import_pokemon(){
   let imp_pokemon;
 
   // Met dans un tableau tous les types disponibles.
-  for(let type in type_effectiveness){
+  for (let type in type_effectiveness) {
     tab_nom_types.push(type);
   }
 
   // Remplit les tableaux des identifiants d'attaques chargées et rapides.
-  for(let attack_c of charged_moves){
+  for (let attack_c of charged_moves) {
     tab_id_attak_c.push(attack_c);
   }
 
-  for(let attack_f of fast_moves){
+  for (let attack_f of fast_moves) {
     tab_id_attak_f.push(attack_f);
   }
 
   // Crée des instances de Type pour chaque type et les stocke dans un objet.
-  for(let nom_type of tab_nom_types){
+  for (let nom_type of tab_nom_types) {
     obj_type = new Type(nom_type, type_effectiveness[nom_type]);
     tab_types[nom_type] = obj_type;
   }
   Type.all_types = tab_types;
 
   // Crée des instances d'Attack pour chaque attaque chargée et les stocke dans un objet.
-  for(let attack_c of tab_id_attak_c){
+  for (let attack_c of tab_id_attak_c) {
     obj_attak_c = new Attack(attack_c.move_id, attack_c.name, attack_c.duration, attack_c.energy_delta, attack_c.power, attack_c.stamina_loss_scaler, Type.all_types[attack_c.type], attack_c.critical_chance || 0, "charged");
     tab_attak_c[attack_c.move_id] = obj_attak_c;
   }
 
   // Crée des instances d'Attack pour chaque attaque rapide et les stocke dans un objet.
-  for(let attack_f of tab_id_attak_f){
+  for (let attack_f of tab_id_attak_f) {
     obj_attak_f = new Attack(attack_f.move_id, attack_f.name, attack_f.duration, attack_f.energy_delta, attack_f.power, attack_f.stamina_loss_scaler, Type.all_types[attack_f.type], 0, "fast");
     tab_attak_f[attack_f.move_id] = obj_attak_f;
   }
@@ -48,43 +48,43 @@ function import_pokemon(){
   Attack.all_attacks = Object.assign({}, tab_attak_f, tab_attak_c);
 
   // Crée des instances de Pokémon en utilisant les données fournies.
-  for(let pok of pokemon){
-    if(pok.form == "Normal"){
-      // Initialise les tableaux pour stocker les types et les attaques des Pokémon.
+  for (let pok of pokemon) {
+    if (pok.form == "Normal") {
+      // Initialise les tableaux pour stocker les types et les attaques des Pokémons.
       types_raw = [];
-      for(let poke_t of pokemon_types){
-        if(poke_t.form == "Normal" && poke_t.pokemon_id == pok.pokemon_id){
+      for (let poke_t of pokemon_types) {
+        if (poke_t.form == "Normal" && poke_t.pokemon_id == pok.pokemon_id) {
           types_raw = poke_t.type
         }
       }
 
       types = []
-      for(let t of types_raw){
+      for (let t of types_raw) {
         types.push(Type.all_types[t])
       }
 
       moves_raw = []
-      for(let poke_m of pokemon_moves){
-        if(poke_m.form == "Normal" && poke_m.pokemon_id == pok.pokemon_id){
+      for (let poke_m of pokemon_moves) {
+        if (poke_m.form == "Normal" && poke_m.pokemon_id == pok.pokemon_id) {
           moves_raw.push(poke_m.charged_moves.concat(poke_m.elite_charged_moves, poke_m.fast_moves, poke_m.elite_fast_moves))
         }
       }
-      
+
       moves = []
-      for(let m of moves_raw[0]){
+      for (let m of moves_raw[0]) {
         let id_attack;
-        for(attack in Attack.all_attacks){
-          if(Attack.all_attacks[attack].nom == m){
+        for (attack in Attack.all_attacks) {
+          if (Attack.all_attacks[attack].nom == m) {
             id_attack = Attack.all_attacks[attack].id
             break;
           }
         }
-        
+
         moves.push(Attack.all_attacks[id_attack])
       }
-      
+
       // Crée une instance de Pokémon avec les données récupérées.
-      imp_pokemon = new Pokemon(pok.pokemon_id,pok.pokemon_name,pok.base_defense,pok.base_attack,pok.base_stamina,types,moves);
+      imp_pokemon = new Pokemon(pok.pokemon_id, pok.pokemon_name, pok.base_defense, pok.base_attack, pok.base_stamina, types, moves);
       tab_poke[pok.pokemon_id] = imp_pokemon;
     }
   }
