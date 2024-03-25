950c1aad5e4f78c6ae05e2e4a872a0832d9e6212
LECONTE ESTEBAN <esleconte@mx21>
2024-03-25 15:53:19 +0100

    v1

--- html/v1/script_v1.js
+++ html/v1/script_v1.js
@@ -8,6 +8,6 @@ identifiant unique
 ● points de défense de base
 ● image (en taille miniature, adaptée à la hauteur des autres champs
 d’information)
-Ce document est mis à disposition selon les termes de la licence Creative Commons “Attribution - Pas
-d’utilisation commerciale - Partage dans les mêmes conditions 3.0 non transposé”
-Adaptation d’un sujet d’A. Delhay par G. Quiniou & J-C. Dubois - Révision 3 - 03-2024 2/4*/
+*/
+
+
