b3b62583751731857aac676b6469a800c4fae2d9
LECONTE ESTEBAN <esleconte@mx21>
2024-03-25 16:46:27 +0100

    v2

--- html/v2/script_v2.js
+++ html/v2/script_v2.js
@@ -1,40 +1,55 @@
-/*
-identifiant unique
-● nom
-● génération
-● types (liste des noms des types du Pokémon)
-● endurance (stamina)
-● points d’attaque de base
-● points de défense de base
-● image (en taille miniature, adaptée à la hauteur des autres champs
-d’information)
-*/
+
+var body_table = document.getElementById('pokemonBody')
+
+for(let poke in Pokemon.all_pokemons){
+    let ligne = document.createElement('tr')
+    body_table.appendChild(ligne)
+
+    let id = document.createElement('td')
+    id.textContent = Pokemon.all_pokemons[poke].pokemon_id
+    ligne.appendChild(id)
+
+    let nom = document.createElement('td')
+    nom.textContent = Pokemon.all_pokemons[poke].pokemon_name
+    ligne.appendChild(nom)
+
+    let gen = document.createElement('td')
+    gen.textContent = Pokemon.all_pokemons[poke].generation
+    ligne.appendChild(gen)
+
+    let types = document.createElement('td')
+    if(Pokemon.all_pokemons[poke].types.length === 1){
+        types.textContent = Pokemon.all_pokemons[poke].types[0].nom
+    }
+    else{
+        types.textContent = `${Pokemon.all_pokemons[poke].types[0].nom} / ${Pokemon.all_pokemons[poke].types[1].nom}`
+    }
+    ligne.appendChild(types)
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const itemsPerPage = 25;
-    let currentPage = 1;
-    let pokemonsData = [];
+    let stam = document.createElement('td')
+    stam.textContent = Pokemon.all_pokemons[poke].base_stamina
+    ligne.appendChild(stam)
 
-    // Fonction pour afficher les Pokémons de la page spécifiée
-   
-        document.getElementById('pageInfo').innerText = `Page ${currentPage} / ${Math.ceil(pokemonsData.length / itemsPerPage)}`;
-    
-    // Gestion des clics sur les boutons de pagination
-    document.getElementById('prevButton').addEventListener('click', function () {
-        if (currentPage > 1) {
-            currentPage--;
-            displayPokemons(currentPage);
-        }
-    });
+    let atk = document.createElement('td')
+    atk.textContent = Pokemon.all_pokemons[poke].base_attack
+    ligne.appendChild(atk)
 
-    document.getElementById('nextButton').addEventListener('click', function () {
-        if (currentPage < Math.ceil(pokemonsData.length / itemsPerPage)) {
-            currentPage++;
-            displayPokemons(currentPage);
-        }
-    });
+    let def = document.createElement('td')
+    def.textContent = Pokemon.all_pokemons[poke].base_defense
+    ligne.appendChild(def)
 
-    // Initialisation de la pagination au chargement de la page
-    initPagination();
-});
+    let img = document.createElement('td')
+    let sprite = document.createElement('img')
+    let img_url = String(Pokemon.all_pokemons[poke].pokemon_id)
+    while(img_url.length < 3){
+        img_url = '0' + img_url
+    }
+
+    sprite.src = `../webp/sprites/${img_url}MS.webp`
+    img.appendChild(sprite)
+    ligne.appendChild(img)
+
+
+
+}
